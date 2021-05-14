56715fb60c4baf8a10247bc15d0b9db0ce7c780c
importamt <dlaqhxoa@gmail.com>
2021-05-14 15:57:47 +0900

    mod - Set production mode

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,7 +1,7 @@
 module.exports = {
     devtool: 'source-map', // 소스맵을 통해 디버깅이 가능하게 해준다.
-    mode: 'development',
-    // mode: 'production',
+    // mode: 'development',
+    mode: 'production',
     entry: { gbPlayer: './index.ts' },
     optimization: {
         concatenateModules: true, // 성능 최적화
